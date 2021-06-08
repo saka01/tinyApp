1e46a859f69bf001a43f34d340db07af7eca40c7
Saka Moshood <sakamoshood@ymail.com>
2021-06-08 20:46:16 +0000

    some stuff with urls

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,10 +13,17 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]/* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
+
 app.listen(PORT, () => {
-  console.log(`Server running on PORT:${PORT}...🚀🚀🚀`);
+  console.log(`Server running on PORT:${PORT}...🚀`);
 });
